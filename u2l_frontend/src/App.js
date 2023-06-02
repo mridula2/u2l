cf1dc95d441096c9adcc8ecc33662e1c110eb9ff
Shubham Suryakant Khulpe <shubham.suryakant-khulpe@hpe.com>
2023-06-02 10:44:29 +0530

    Creted new login page

--- u2l_frontend/src/App.js
+++ u2l_frontend/src/App.js
@@ -1,12 +1,18 @@
-import AppRouting from "./routes/AppRouting";
-import { Grommet, ResponsiveContext } from "grommet";
-import { hpe } from "grommet-theme-hpe";
+import AppRouting from './routes/AppRouting';
+import { Grommet, ResponsiveContext, Box } from 'grommet';
+import { hpe } from 'grommet-theme-hpe';
+import AppHeader from './components/navbars/AppHeader';
 
 function App() {
   return (
     <Grommet theme={hpe} full>
       <ResponsiveContext.Consumer>
-        {(size) => <AppRouting />}
+        {(size) => (
+          <Box>
+            <AppHeader />
+            <AppRouting />
+          </Box>
+        )}
       </ResponsiveContext.Consumer>
     </Grommet>
   );
