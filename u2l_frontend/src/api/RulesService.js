c203703f4c13f2551fa731b8969f49b57abcc69f
pratikwani <pratikjwani@gmail.com>
2024-02-15 16:32:06 +0530

    [pratik] : frontend code added

--- u2l_frontend/src/api/RulesService.js
+++ u2l_frontend/src/api/RulesService.js
@@ -4,29 +4,47 @@ const addRules = (data) => {
   const config = {
     method: 'post',
     maxBodyLength: Infinity,
-    url: `/rules/add`,
+    url: `/collectdata`,
     headers: {
       'Content-Type': 'application/json',
     },
     data: {
-      ...data,
+      java_doc_version: `${data.package_version}`,
     },
   };
   return axios(config);
 };
 
-const checkURL = () => {
+const addJavaLibraries = (data) => {
   const config = {
-    method: 'head',
+    method: 'post',
     maxBodyLength: Infinity,
-    url: '/checkurl',
-    
+    url: `/collect_other_libraries`,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: {
+      package_language: `${data.package_language}`,
+      package_url: `${data.package_url}`,
+      package_name: `${data.package_name}`,
+      package_version: `${data.package_version}`,
+    },
+  };
+  return axios(config);
+};
+
+const checkURL = (url) => {
+  const config = {
+    method: 'post',
+    url: '/check_website',
+    data: { url: url },
   };
   return axios(config);
 };
 const RulesService = {
   addRules,
   checkURL,
+  addJavaLibraries,
 };
 
 export default RulesService;
--- u2l_frontend/src/pages/Rules.js
+++ u2l_frontend/src/pages/Rules.js
@@ -14,8 +14,9 @@ import {
   TextInput,
   Select,
   Notification,
+  RangeInput,
 } from 'grommet';
-import { Compliance, Close, Checkmark } from 'grommet-icons';
+import { Compliance, Close, Checkmark, Java } from 'grommet-icons';
 
 import SideBar from '../components/common/SideBar';
 import Colors from '../config/colors';
@@ -37,12 +38,18 @@ const AddRulesForm = ({
     'C++/Pro*C',
   ];
   const [formValues, setFormValues] = useState({
-    language: '',
-    url: '',
-    packageName: '',
-    version: '',
+    package_language: '',
+    package_url: '',
+    package_name: '',
+    package_version: '',
   });
+  const [packageLang, setPackageLang] = useState('Java');
   const [isServerReachable, setIsServerReachable] = useState(false);
+
+  const handleshowhide = (event) => {
+    const getLang = event.target.value;
+    setPackageLang(getLang);
+  };
 
   const checkServerStatus = () => {
     RulesService.checkURL().then((response) => {
@@ -55,40 +62,58 @@ const AddRulesForm = ({
   };
   const onSubmit = () => {
     console.log(formValues);
-    checkServerStatus();
-    // console.log(check);
-    setNotificationVisible(true);
-    setNotificationMessage(`Checking server status`);
-    setStatus('info');
-    if (isServerReachable) {
-      // API call
-      setNotificationVisible(true);
-      setNotificationMessage(
-        `Adding rules for package ${formValues.packageName}`
-      );
-      setStatus('info');
-      RulesService.addRules(formValues)
-        .then((response) => {
-          console.log(response);
-          setNotificationVisible(true);
-          setNotificationMessage(
-            `Rules for package ${formValues.packageName} added successfully`
-          );
-          setStatus('normal');
-        })
-        .catch((err) => {
-          console.log(err);
-          setNotificationVisible(true);
-          setNotificationMessage(
-            `Failed to add rules for package ${formValues.packageName}`
-          );
-          setStatus('critical');
-        });
-    } else {
-      setNotificationVisible(true);
-      setNotificationMessage(`Website is not reachable!`);
-      setStatus('critical');
-    }
+
+    // checkServerStatus();
+    // // console.log(check);
+    // setNotificationVisible(true);
+    // setNotificationMessage(`Checking server status`);
+    // setStatus('info');
+    // if (isServerReachable) {
+    //   // API call
+    //   setNotificationVisible(true);
+    //   setNotificationMessage(
+    //     `Adding rules for package ${formValues.packageName}`
+    //   );
+    //   setStatus('info');
+    //   RulesService.addRules(formValues)
+    //     .then((response) => {
+    //       console.log(response);
+    //       setNotificationVisible(true);
+    //       setNotificationMessage(
+    //         `Rules for package ${formValues.packageName} added successfully`
+    //       );
+    //       setStatus('normal');
+    //     })
+    //     .catch((err) => {
+    //       console.log(err);
+    //       setNotificationVisible(true);
+    //       setNotificationMessage(
+    //         `Failed to add rules for package ${formValues.packageName}`
+    //       );
+    //       setStatus('critical');
+    //     });
+    // } else {
+    //   setNotificationVisible(true);
+    //   setNotificationMessage(`Website is not reachable!`);
+    //   setStatus('critical');
+    // }
+    RulesService.addRules(formValues)
+      .then((response) => {
+        console.log(response);
+        setNotificationVisible(true);
+        setNotificationMessage(
+          `Rules for package ${formValues.package_name} added successfully`
+        );
+        setStatus('normal');
+      })
+      .catch((err) => {
+        console.log(err);
+        setNotificationVisible(true);
+        setNotificationMessage(
+          `Failed to add rules for package ${formValues.package_name}`
+        );
+        setStatus('critical');
+      });
     closeLayer();
   };
   return (
@@ -125,74 +150,328 @@ const AddRulesForm = ({
           </Text> */}
             <FormField
               label='Language'
-              htmlFor='language'
-              name='language'
+              htmlFor='package_language'
+              name='package_language'
               required={true}
             >
               <Select
                 placeholder='Languages'
-                id='language'
-                name='language'
+                id='package_language'
+                name='package_language'
                 options={language}
+                onChange={(e) => handleshowhide(e)}
               />
             </FormField>
+            {packageLang !== 'Java' && (
+              <>
+                <Box>
+                  <Box direction='row'>
+                    <Box fill>
+                      <FormField
+                        label='URL'
+                        htmlFor='package_url'
+                        name='package_url'
+                        // validate={emailValidation}
+                        required={true}
+                        // onBlur={checkServerStatus}
+                        margin={{ right: 'small' }}
+                      >
+                        <TextInput
+                          id='package_url'
+                          name='package_url'
+                          type='package_url'
+                          placeholder='https://www.langdocs.com'
+                        />
+                      </FormField>
+                    </Box>
+                    <Box>
+                      {!isServerReachable && (
+                        <Close style={{ marginTop: '37px' }} color='red' />
+                      )}
+                      {isServerReachable && (
+                        <Checkmark
+                          color='green'
+                          style={{ marginTop: '37px' }}
+                        />
+                      )}
+                    </Box>
+                  </Box>
+                  {!isServerReachable && (
+                    <Text color={'red'}>Please Enter valid url</Text>
+                  )}
+                </Box>
+                <FormField
+                  label='Package Name'
+                  htmlFor='package_name'
+                  name='package_name'
+                  // validate={emailValidation}
+                  required={true}
+                >
+                  <TextInput
+                    id='package_name'
+                    name='package_name'
+                    type='text'
+                    placeholder='Enter Package Name'
+                  />
+                </FormField>
+              </>
+            )}
+            <FormField
+              label='Version'
+              htmlFor='package_version'
+              name='package_version'
+              // validate={emailValidation}
+              required={true}
+            >
+              <TextInput
+                id='package_version'
+                name='package_version'
+                type='number'
+                placeholder='Version to be added'
+                min={6}
+                max={19}
+              />
+            </FormField>
+            <Button
+              label='Add Rules'
+              primary
+              type='submit'
+              margin={{ top: 'small' }}
+              // disabled={!isServerReachable}
+            />
+          </Box>
+        </Form>
+      </Box>
+    </Box>
+  );
+};
+
+const AddJavaLibrariesForm = ({
+  closeLayer,
+  setNotificationVisible,
+  setNotificationMessage,
+  setStatus,
+}) => {
+  const [formValues, setFormValues] = useState({
+    package_name: '',
+    package_version: '',
+    package_url: '',
+  });
+
+  const packages = [
+    'Angus Activation',
+    'ECJ',
+    'JavaHamcrest',
+    'Mchange commons',
+    'Micrometer Core',
+    'okhttp',
+    'zip4j',
+    'Apache Tomcat',
+    'Apache ANT',
+    'aspectjweaver',
+    'aspectjrt',
+    'Axis',
+    'Bouncycastle',
+    'cglib-nodep',
+    'Commons Beanutils',
+    'Commons Configuration',
+    'Commons Dbcp',
+    'Commons Digester',
+    'Commons Discovery',
+    'Commons Lang',
+    'Commons Collections',
+    'Commons Codec',
+    'Apache Commons',
+    'Commons Pool',
+    'Commons Logging',
+    'Diffutils',
+    'Geronimo',
+    'Groovy-lang',
+    'Hazelcast',
+    'HTTPcore',
+    'HTTPclient',
+    'Imap',
+    'IText',
+    'Jasper Reports',
+    'Jaxrpc',
+    'Jcifs',
+    'Jcommon',
+    'Jdom',
+    'Jfreechart',
+    'Jsch Documentation',
+    'Juddi Client',
+    'Apache OpenJPA',
+    'Mybatis',
+    'Mybatis Spring',
+    'Opencsv',
+    'POI',
+    'Quartz scheduler',
+    'slf4j',
+    'wsdl4j',
+    'XMLBeans',
+  ];
+  const [isServerReachable, setIsServerReachable] = useState();
+  const [options, setOptions] = useState(packages);
+
+  const checkServerStatus = (url) => {
+    RulesService.checkURL(url)
+      .then((response) => {
+        if (response.status === 200) {
+          setIsServerReachable(true);
+        } else {
+          setIsServerReachable(false);
+        }
+        console.log(response);
+      })
+      .catch((error) => {
+        setNotificationMessage('error');
+        setNotificationVisible(true);
+        setStatus('critical');
+      });
+  };
+
+  const onSubmit = () => {
+    // checkServerStatus(formValues.package_url);
+    console.log(formValues);
+    const checkURL =
+      formValues.package_url !== null && formValues.package_url !== '';
+    if (checkURL) {
+      checkServerStatus(formValues.package_url);
+    }
+    setNotificationVisible(true);
+    setNotificationMessage(`Checking server status`);
+    setStatus('info');
+    if ((checkURL && isServerReachable) || checkURL === false) {
+      // API call
+      setNotificationVisible(true);
+      setNotificationMessage(
+        `Adding Libraries for package ${formValues.package_name}`
+      );
+      setStatus('info');
+      RulesService.addJavaLibraries(formValues)
+        .then((response) => {
+          // console.log(response);
+          setNotificationVisible(true);
+          setNotificationMessage(`Libraries added successfully`);
+          setStatus('normal');
+        })
+        .catch((err) => {
+          console.log(err);
+          setNotificationVisible(true);
+          setNotificationMessage(`Failed to add libraries`);
+          setStatus('critical');
+        });
+    } else if (checkURL && !isServerReachable) {
+      setNotificationVisible(true);
+      setNotificationMessage(`Website is not reachable!`);
+      setStatus('critical');
+    }
+    closeLayer();
+  };
+  return (
+    <Box>
+      <Box
+        direction='row'
+        justify='end'
+        pad={{ horizontal: 'small', top: 'small' }}
+      >
+        <Button a11yTitle='Close form' icon={<Close />} onClick={closeLayer} />
+      </Box>
+      <Box
+        gap='medium'
+        margin={{ horizontal: 'large', bottom: 'large', top: 'small' }}
+        width='medium'
+        style={{ overflow: 'clip' }}
+      >
+        <Heading level={2} margin='none'>
+          Add Java Libraries
+        </Heading>
+        <Form
+          validate='submit'
+          value={formValues}
+          messages={{
+            required: 'This is a required field.',
+          }}
+          onChange={setFormValues}
+          onSubmit={({ value, touched }) => onSubmit({ value, touched })}
+          method='post'
+        >
+          <Box gap='small'>
+            {/* <Text>
+            An email to reset your password will be sent to the following
+            address:
+          </Text> */}
+            <FormField
+              label='Package Name'
+              htmlFor='package_name'
+              name='package_name'
+              required={true}
+            >
+              <Select
+                placeholder='Package Name'
+                id='package_name'
+                name='package_name'
+                options={options}
+                onClose={() => setOptions(packages)}
+                onSearch={(text) => {
+                  const escapedText = text.replace(
+                    /[-\\^$*+?.()|[\]{}]/g,
+                    '\\$&'
+                  );
+                  const exp = new RegExp(escapedText, 'i');
+                  setOptions(packages.filter((o) => exp.test(o)));
+                }}
+              />
+            </FormField>
+
             <Box>
               <Box direction='row'>
                 <Box fill>
                   <FormField
                     label='URL'
-                    htmlFor='url'
-                    name='url'
+                    htmlFor='package_url'
+                    name='package_url'
                     // validate={emailValidation}
-                    required={true}
+                    // required={true}
                     // onBlur={checkServerStatus}
                     margin={{ right: 'small' }}
                   >
                     <TextInput
-                      id='url'
-                      name='url'
-                      type='url'
+                      id='package_url'
+                      name='package_url'
+                      type='package_url'
                       placeholder='https://www.langdocs.com'
                     />
                   </FormField>
                 </Box>
                 <Box>
-                  {!isServerReachable && (
-                    <Close style={{ marginTop: '37px' }} color='red' />
-                  )}
+                  {isServerReachable !== undefined &&
+                    isServerReachable !== null &&
+                    isServerReachable === false && (
+                      <Close style={{ marginTop: '37px' }} color='red' />
+                    )}
                   {isServerReachable && (
                     <Checkmark color='green' style={{ marginTop: '37px' }} />
                   )}
                 </Box>
               </Box>
-              {!isServerReachable && (
-                <Text color={'red'}>Please Enter valid url</Text>
-              )}
+              {isServerReachable !== undefined &&
+                isServerReachable !== null &&
+                isServerReachable === false && (
+                  <Text color={'red'}>Please Enter valid url</Text>
+                )}
             </Box>
-            <FormField
-              label='Package Name'
-              htmlFor='packageName'
-              name='packageName'
-              // validate={emailValidation}
-              required={true}
-            >
-              <TextInput
-                id='packageName'
-                name='packageName'
-                type='text'
-                placeholder='Enter Package Name'
-              />
-            </FormField>
+
             <FormField
               label='Version'
-              htmlFor='version'
-              name='version'
+              htmlFor='package_version'
+              name='package_version'
               // validate={emailValidation}
               required={true}
             >
               <TextInput
-                id='version'
-                name='version'
+                id='package_version'
+                name='package_version'
                 type='text'
                 placeholder='Version to be added'
               />
@@ -202,7 +481,9 @@ const AddRulesForm = ({
               primary
               type='submit'
               margin={{ top: 'small' }}
-              disabled={!isServerReachable}
+              disabled={
+                isServerReachable === null || isServerReachable === false
+              }
             />
           </Box>
         </Form>
@@ -212,7 +493,8 @@ const AddRulesForm = ({
 };
 
 const Rules = () => {
-  const [showAddRules, setShowAddRules] = useState(false);
+  const [showAddLanguageRules, setShowAddLanguageRules] = useState(false);
+  const [showJavaLibrariesRules, setShowJavaLibrariesRules] = useState(false);
   const [notificationVisible, setNotificationVisible] = useState();
   const [notificationMessage, setNotificationMessage] = useState('');
   const [status, setStatus] = useState('unknown');
@@ -223,13 +505,20 @@ const Rules = () => {
     setNotificationVisible(false);
   };
   const onAddRules = () => {
-    setShowAddRules(!showAddRules);
+    setShowAddLanguageRules(!showAddLanguageRules);
   };
-  const onClose = () => {
-    setShowAddRules(false);
+  const onCloseRules = () => {
+    setShowAddLanguageRules(false);
+  };
+
+  const onAddJavaLibraries = () => {
+    setShowJavaLibrariesRules(!showJavaLibrariesRules);
+  };
+  const onCloseJavaLibraries = () => {
+    setShowJavaLibrariesRules(false);
   };
   return (
-    <Box direction='row-responsive' responsive={true} flex='shrink'>
+    <Box direction='row'>
       <Box align='center' gap='small'>
         {notificationVisible && (
           <Notification
@@ -268,12 +557,47 @@ const Rules = () => {
             <Text>Click to add rules</Text>
           </Box>
         </Card>
+
+        <Card
+          pad='medium'
+          width='27%'
+          margin='small'
+          height='35vh'
+          style={{ cursor: 'pointer' }}
+          onClick={onAddJavaLibraries}
+        >
+          <Java size='xlarge' color={Colors.primaryBlue400} />
+          <Header
+            style={{ fontWeight: 'bold', fontSize: '24px' }}
+            margin={{ top: 'medium' }}
+          >
+            Add Java Libraries
+          </Header>
+          <Box direction='row' margin={{ top: 'medium' }}>
+            <Text>Click to add rules</Text>
+          </Box>
+        </Card>
       </Box>
+
       <Box align='start' margin={{ top: 'medium', bottom: 'small' }}>
-        {showAddRules && (
-          <Layer modal onClickOutside={onClose} onEsc={onClose}>
+        {showAddLanguageRules && (
+          <Layer modal onClickOutside={onCloseRules} onEsc={onCloseRules}>
             <AddRulesForm
-              closeLayer={onClose}
+              closeLayer={onCloseRules}
+              setNotificationVisible={setNotificationVisible}
+              setNotificationMessage={setNotificationMessage}
+              setStatus={setStatus}
+            />
+          </Layer>
+        )}
+        {showJavaLibrariesRules && (
+          <Layer
+            modal
+            onClickOutside={onCloseJavaLibraries}
+            onEsc={onCloseJavaLibraries}
+          >
+            <AddJavaLibrariesForm
+              closeLayer={onCloseJavaLibraries}
               setNotificationVisible={setNotificationVisible}
               setNotificationMessage={setNotificationMessage}
               setStatus={setStatus}
